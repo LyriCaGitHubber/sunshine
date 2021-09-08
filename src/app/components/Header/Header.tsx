27bdec554342a6c5744a494466794554b859b78d
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-08 10:48:07 +0200

    🔥 Remove Types for header component

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styles from './Header.module.css';
 import SunIcon from '../../assets/SunIcon';
 
-export type HeaderProps = {
-  headline: string;
-};
-
 export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
