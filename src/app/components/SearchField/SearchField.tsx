8dc9e09d8c135afc6b62638340e6ff1c8074b3c1
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-10 21:48:43 +0200

    ✨ Create SearchField component

---
+++ src/app/components/SearchField/SearchField.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './SearchField.module.css';
+import ButtonLarge from '../ButtonLarge/ButtonLarge';
+
+type SearchFieldProps = {
+  type: 'Search' | 'Add';
+  locationSearchValue: string;
+  setLocationSearchValue: (locationSearchValue: string) => void;
+  handleSubmit: (event: React.FormEvent) => void;
+};
+
+export default function SearchField({
+  type,
+  locationSearchValue,
+  setLocationSearchValue,
+  handleSubmit,
+}: SearchFieldProps): JSX.Element {
+  return (
+    <form className={styles.formSection} onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          value={locationSearchValue}
+          className={styles.formInput}
+          placeholder={
+            type === 'Search' ? 'Search' : type === 'Add' ? 'Add' : `${type}`
+          }
+          onChange={(event) => {
+            event.preventDefault();
+            setLocationSearchValue(event.target.value);
+          }}
+        />
+      </label>
+      <ButtonLarge children={type === 'Search' ? 'Search' : 'Add'} />
+    </form>
+  );
+}
