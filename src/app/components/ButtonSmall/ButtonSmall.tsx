8e88ca325d12eb96b30d47e09b59e6b37e733f54
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-13 12:15:00 +0200

    ✨ Create small button component

---
+++ src/app/components/ButtonSmall/ButtonSmall.tsx
@@ -0,0 +1,22 @@
+import React, { ReactChild, ReactNode } from 'react';
+import styles from './ButtonSmall.module.css';
+
+type ButtonSmall = {
+  type: 'weather' | 'delete';
+  children: ReactNode;
+};
+
+export default function ButtonSmall({
+  type,
+  children,
+}: ButtonSmall): JSX.Element {
+  return (
+    <button
+      className={`${styles.buttonSmall} ${
+        type === 'weather' ? styles.weather : styles.delete
+      }`}
+    >
+      {children}
+    </button>
+  );
+}
