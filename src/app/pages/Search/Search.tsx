77ff65a5a9414b3829d4eb1da47e14130a94fec2
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-14 18:41:36 +0200

    ✨ Create search page

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar';
+import SearchField from '../../components/SearchField/SearchField';
 import styles from './Search.module.css';
 
 export default function Search(): JSX.Element {
   return (
     <div className={styles.container}>
       <Header />
+      <SearchField
+        type="Search"
+        locationSearchValue="München"
+        setLocationSearchValue={() => console.log('yo passt')}
+        handleSubmit={() => console.log('submit')}
+      />
+      <Navbar LinkActive="search" />
     </div>
   );
 }
