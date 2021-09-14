050fa82f163561dd74798dc81fcb191aa08bceab
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-14 16:13:12 +0200

    ✨ Create home page

---
+++ src/app/pages/Home/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Home.module.css';
+import Header from '../../components/Header/Header';
+import LocationCard from '../../components/LocationCard/LocationCard';
+import Navbar from '../../components/Navbar/Navbar';
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <Header></Header>
+      <p className={styles.userInfo}>Your location:</p>
+      <LocationCard
+        locationName="München"
+        degree={25}
+        icon="//cdn.weatherapi.com/weather/64x64/day/113.png"
+        description="windig"
+      />
+      <Navbar LinkActive="home" />
+    </div>
+  );
+}
