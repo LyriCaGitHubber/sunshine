6da8817bc13ccd279099dcb0684d2955c9d128f1
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-15 15:51:22 +0200

    ✨ Create location text stories

---
+++ src/app/components/LocationText/LocationText.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LocationText from './LocationText';
+
+export default {
+  title: 'Component/LocationText',
+  component: LocationText,
+};
+
+export const LocationTextExample = (): JSX.Element => (
+  <LocationText locationName="Karlsfeld" />
+);
