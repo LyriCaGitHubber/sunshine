423d5fbef74d9099e4d8a40d839775be7484761d
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-16 10:11:56 +0200

    🚑 Fix static image import

--- src/app/components/LocationText/LocationText.tsx
+++ src/app/components/LocationText/LocationText.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import styles from './LocationText.module.css';
+import locationIcon from '../../assets/location-icon-white.svg';
 
 type LocationTextProps = {
   locationName: ReactNode;
@@ -10,7 +11,7 @@ export default function LocationText({
 }: LocationTextProps): JSX.Element {
   return (
     <p className={styles.locationField}>
-      <img src="src/app/assets/location-icon-white.svg" />
+      <img src={locationIcon} />
       {locationName}
     </p>
   );
