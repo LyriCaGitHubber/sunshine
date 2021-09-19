78d9fd8b7b5d563cb86ea2f2b948bcf486ec6ec1
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-19 23:55:28 +0200

    ♻️ Move useRevGeoCoding hook into location card

--- src/app/components/LocationCard/LocationCard.tsx
+++ src/app/components/LocationCard/LocationCard.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import useRevGeoCoding from '../../hooks/useRevGeoCoding';
 import styles from './LocationCard.module.css';
 
 export type LocationCardProps = {
-  locationName: string | null;
   degree: number;
   icon: string;
   description: string;
 };
 
 export default function LocationCard({
-  locationName,
   degree,
   icon,
   description,
 }: LocationCardProps): JSX.Element {
+  const cityName = useRevGeoCoding();
+
   return (
     <section className={styles.locationCard}>
-      <p>{locationName}</p>
+      <p>{cityName}</p>
       <article className={styles.locationCard__info}>
         <p>{degree}°</p>
         <img src={icon} />
--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Home.module.css';
 import Header from '../../components/Header/Header';
 import LocationCard from '../../components/LocationCard/LocationCard';
-import useRevGeoCoding from '../../hooks/useRevGeoCoding';
 import Navbar from '../../components/Navbar/Navbar';
 
 export default function Home(): JSX.Element {
@@ -11,7 +10,6 @@ export default function Home(): JSX.Element {
       <Header></Header>
       <p className={styles.userInfo}>Your location:</p>
       <LocationCard
-        locationName={useRevGeoCoding()}
         degree={25}
         icon="//cdn.weatherapi.com/weather/64x64/day/113.png"
         description="sonnig"
