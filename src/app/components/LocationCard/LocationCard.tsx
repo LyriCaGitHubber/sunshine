a3c188dba6f7025f2a5ba1445bc3570c4aea5b70
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-20 21:15:59 +0200

    ♻️ Add wind property to location card

--- src/app/components/LocationCard/LocationCard.tsx
+++ src/app/components/LocationCard/LocationCard.tsx
@@ -5,6 +5,7 @@ export type LocationCardProps = {
   locationName: string | undefined;
   degree: string | undefined;
   icon: string | undefined;
+  wind: number | null | undefined;
   description: string | undefined;
 };
 
@@ -12,14 +13,21 @@ export default function LocationCard({
   locationName,
   degree,
   icon,
+  wind,
   description,
 }: LocationCardProps): JSX.Element {
   return (
     <section className={styles.locationCard}>
       <p>{locationName}</p>
       <article className={styles.locationCard__info}>
-        <p>{degree}°</p>
-        <img src={icon} />
+        <div className={styles.infoValues}>
+          <p>{degree}°</p>
+          <img src={icon} />
+          <p>
+            Wind:
+            <br /> {wind} km/h
+          </p>
+        </div>
         <p className={styles.weatherDescription}>{description}</p>
       </article>
     </section>
