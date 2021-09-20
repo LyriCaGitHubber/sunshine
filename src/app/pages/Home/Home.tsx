60ff8ce3dae688fcb5c71403553d081383863e6b
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-20 19:02:33 +0200

    ♻️ Implement useWeather hook into home page

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import styles from './Home.module.css';
 import useRevGeoCoding from '../../hooks/useRevGeoCoding';
+import useWeather from '../../hooks/useWeather';
 import Header from '../../components/Header/Header';
 import LocationCard from '../../components/LocationCard/LocationCard';
 import Navbar from '../../components/Navbar/Navbar';
 
 export default function Home(): JSX.Element {
   const { cityName, cityIsLoading } = useRevGeoCoding();
+  const data = useWeather();
+
   return (
     <div className={styles.container}>
       <Header></Header>
       <p className={styles.userInfo}>Your location:</p>
-      {cityIsLoading ? (
+      {cityIsLoading && data ? (
         <p className={styles.loadingText}>Loading...</p>
       ) : (
         <LocationCard
           locationName={cityName}
-          degree={25}
-          icon="//cdn.weatherapi.com/weather/64x64/day/113.png"
-          description="windig"
+          degree={data.degree}
+          icon={data.icon}
+          description={data.description}
         />
       )}
       <Navbar LinkActive="home" />
