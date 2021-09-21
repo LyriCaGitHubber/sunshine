29884d4923a02367b4b567a58f04beba5fd4816c
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-21 20:21:44 +0200

    ✨ Implement useHistory to forward on btn click

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import useForecast from '../../hooks/useForecast';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import SearchField from '../../components/SearchField/SearchField';
@@ -6,6 +8,8 @@ import styles from './Search.module.css';
 
 export default function Search(): JSX.Element {
   const [searchValue, setSearchValue] = useState('');
+  console.log(useForecast(searchValue));
+  const history = useHistory();
   return (
     <div className={styles.container}>
       <Header />
@@ -13,7 +17,7 @@ export default function Search(): JSX.Element {
         type="Search"
         locationSearchValue={searchValue}
         setLocationSearchValue={setSearchValue}
-        handleSubmit={() => console.log('submit')}
+        handleSubmit={() => history.push('/forecast')}
       />
       <Navbar LinkActive="search" />
     </div>
