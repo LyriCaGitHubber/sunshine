79276836c5c0871d06c57c9a589252069f4f5a75
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-21 17:40:44 +0200

    ♻️ Set values for SearchField components

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import SearchField from '../../components/SearchField/SearchField';
 import styles from './Search.module.css';
 
 export default function Search(): JSX.Element {
+  const [searchValue, setSearchValue] = useState('');
   return (
     <div className={styles.container}>
       <Header />
       <SearchField
         type="Search"
-        locationSearchValue="München"
-        setLocationSearchValue={() => console.log('yo passt')}
+        locationSearchValue={searchValue}
+        setLocationSearchValue={setSearchValue}
         handleSubmit={() => console.log('submit')}
       />
       <Navbar LinkActive="search" />
