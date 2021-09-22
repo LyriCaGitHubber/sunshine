3b675f48f78d543ea05c57100eedd397f4c97633
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-22 21:56:57 +0200

    ♻️ Create function handleSubmit and set location to local storage

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -5,11 +5,20 @@ import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import SearchField from '../../components/SearchField/SearchField';
 import styles from './Search.module.css';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 export default function Search(): JSX.Element {
   const [searchValue, setSearchValue] = useState('');
   const data = useForecast(searchValue);
+  const [cityLocation, setCityLocation] = useLocalStorage('city', searchValue);
   const history = useHistory();
+
+  function handleSubmit(event: React.FormEvent): void {
+    event.preventDefault();
+    setCityLocation(data.cityLocation);
+    history.push('/forecast');
+  }
+
   return (
     <div className={styles.container}>
       <Header />
@@ -17,10 +26,7 @@ export default function Search(): JSX.Element {
         type="Search"
         locationSearchValue={searchValue}
         setLocationSearchValue={setSearchValue}
-        handleSubmit={(event) => {
-          event.preventDefault();
-          history.push('/forecast');
-        }}
+        handleSubmit={handleSubmit}
       />
       <Navbar LinkActive="search" />
     </div>
