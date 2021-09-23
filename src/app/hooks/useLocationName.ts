ed1b64ab5bdffb1ffc2837834fe4e7ef3c7ef155
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-23 21:10:00 +0200

    ✨ Create custom hook useLocationName

---
+++ src/app/hooks/useLocationName.ts
@@ -0,0 +1,14 @@
+import useLocalStorage from './useLocalStorage';
+
+export default function useLocationName(): {
+  cityLocations: string;
+  addCityLocation: (cityLocation: string) => void;
+} {
+  const [cityLocations, setCityLocation] = useLocalStorage('location', '');
+
+  function addCityLocation(cityLocation: string): void {
+    setCityLocation([cityLocation]);
+  }
+
+  return { cityLocations, addCityLocation };
+}
