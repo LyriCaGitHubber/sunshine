a5c04eb7f501d4732b80034f5953a426b88d83a7
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-23 21:12:08 +0200

    ♻️ Use custom hook instead of useLocalStorage

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
-import useForecast from '../../hooks/useForecast';
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import SearchField from '../../components/SearchField/SearchField';
 import styles from './Search.module.css';
-import useLocalStorage from '../../hooks/useLocalStorage';
+import useLocationName from '../../hooks/useLocationName';
 
 export default function Search(): JSX.Element {
   const [searchValue, setSearchValue] = useState('');
-  const data = useForecast(searchValue);
-  const [cityLocation, setCityLocation] = useLocalStorage('city', searchValue);
   const history = useHistory();
+  const { addCityLocation } = useLocationName();
 
   function handleSubmit(event: React.FormEvent): void {
     event.preventDefault();
-    setCityLocation(data.cityLocation);
+    addCityLocation(searchValue);
     history.push('/forecast');
   }
 
