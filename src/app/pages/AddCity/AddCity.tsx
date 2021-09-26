958a00abf8f8e3d872200209cdb50f4888da17f0
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-26 20:43:52 +0200

    ✨ Create state to control SearchField component

--- src/app/pages/AddCity/AddCity.tsx
+++ src/app/pages/AddCity/AddCity.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import SearchField from '../../components/SearchField/SearchField';
 import styles from './AddCity.module.css';
 
 export default function AddCity(): JSX.Element {
+  const [addValue, setAddValue] = useState('');
+  const history = useHistory();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    history.push('/cities');
+  }
+
   return (
     <div className={styles.container}>
       <Header />
       <SearchField
-        locationSearchValue="Germering"
+        locationSearchValue={addValue}
         type="Add"
-        setLocationSearchValue={() => {
-          console.log('Frankfurt');
-        }}
-        handleSubmit={() => console.log('submit')}
+        setLocationSearchValue={setAddValue}
+        handleSubmit={handleSubmit}
       />
       <Navbar LinkActive="city" />
     </div>
