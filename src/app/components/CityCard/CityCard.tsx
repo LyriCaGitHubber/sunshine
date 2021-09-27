27e6e58e0fef1b90113894b0300ab260a32d9508
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-27 11:57:59 +0200

    Enhance city card component with function props

--- src/app/components/CityCard/CityCard.tsx
+++ src/app/components/CityCard/CityCard.tsx
@@ -5,21 +5,25 @@ import styles from './CityCard.module.css';
 type CityCardProps = {
   locationName: string;
   region: string;
+  onDeleteClick: () => void;
+  onWeatherClick: () => void;
 };
 
 export default function CityCard({
   locationName,
   region,
+  onWeatherClick,
+  onDeleteClick,
 }: CityCardProps): JSX.Element {
   return (
     <section className={styles.cityCard}>
       <p>{locationName}</p>
       <p>{region}</p>
       <div className={styles.buttonArea}>
-        <ButtonSmall onclick={() => console.log('sauba sog i')} type="weather">
+        <ButtonSmall onclick={onWeatherClick} type="weather">
           Weather
         </ButtonSmall>
-        <ButtonSmall onclick={() => console.log('sauba basst a')} type="delete">
+        <ButtonSmall onclick={onDeleteClick} type="delete">
           Delete
         </ButtonSmall>
       </div>
