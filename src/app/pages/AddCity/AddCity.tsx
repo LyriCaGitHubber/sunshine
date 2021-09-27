c91202307f003b571a5998b5cd79ff33a426638d
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-27 11:54:18 +0200

    ♻️ Implement custom local storage hook

--- src/app/pages/AddCity/AddCity.tsx
+++ src/app/pages/AddCity/AddCity.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import useAddCity from '../../hooks/useAddCity';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import SearchField from '../../components/SearchField/SearchField';
@@ -8,9 +9,11 @@ import styles from './AddCity.module.css';
 export default function AddCity(): JSX.Element {
   const [addValue, setAddValue] = useState('');
   const history = useHistory();
+  const { addCity } = useAddCity();
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
+    addCity(addValue);
     history.push('/cities');
   }
 
