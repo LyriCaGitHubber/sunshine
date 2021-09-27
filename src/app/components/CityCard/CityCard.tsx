a0a347ed1a463df1536699188dad3e0b521df8e1
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-09-27 17:31:05 +0200

    ♻️ Show region value in city card

--- src/app/components/CityCard/CityCard.tsx
+++ src/app/components/CityCard/CityCard.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
+import useForecast from '../../hooks/useForecast';
 import ButtonSmall from '../ButtonSmall/ButtonSmall';
 import styles from './CityCard.module.css';
 
 type CityCardProps = {
   locationName: string;
-  region: string;
   onDeleteClick: () => void;
   onWeatherClick: () => void;
 };
 
 export default function CityCard({
   locationName,
-  region,
   onWeatherClick,
   onDeleteClick,
 }: CityCardProps): JSX.Element {
+  const data = useForecast(locationName);
   return (
     <section className={styles.cityCard}>
       <p>{locationName}</p>
-      <p>{region}</p>
+      <p>{data?.region}</p>
       <div className={styles.buttonArea}>
         <ButtonSmall onclick={onWeatherClick} type="weather">
           Weather
